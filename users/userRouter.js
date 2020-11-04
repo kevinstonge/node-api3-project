96718e13623989aae7c24fd257f061f0b25d06d6
Kevin St.Onge <kevinstonge@gmail.com>
2020-11-04 10:15:13 -0500

    validateUser working

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,14 +1,25 @@
 const express = require('express');
 
 const router = express.Router();
-
+router.use(express.json());
 const user = require('./userDb.js');
 
-router.post('/', (req, res) => {
-  console.log('post -> new user')
+router.use('/:id', validateUserId);
+
+router.post('/', validateUser, (req, res) => {
+  user.insert(req.body)
+    .then(r => {
+      res.status(200).json({
+        message: "user created",
+        user: r
+      })
+    })
+    .catch(e => {
+      res.status(500).json({message: "unable to create user"})
+    })
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', (req, res) => {
   console.log('post to user by id');
   res.status(200).json({message:"hello"})
 });
@@ -17,12 +28,12 @@ router.get('/', (req, res) => {
   console.log('get users');
 });
 
-router.get('/:id', validateUserId, (req, res) => {
+router.get('/:id', (req, res) => {
   console.log('get user by id');
   res.status(200).json({ message: "get user by id" });
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', (req, res) => {
   console.log('get posts by user id');
   res.status(200).json({
     message: "get posts by user id",
@@ -30,11 +41,11 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   });
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
+router.delete('/:id', (req, res) => {
   console.log('delete user by id')
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', (req, res) => {
   console.log('update user by id')
 });
 
@@ -49,7 +60,7 @@ function validateUserId(req, res, next) {
         next();
       }
       else {
-        res.status(400).json({message: "invalid user id"})
+        res.status(404).json({message: "invalid user id"})
       }
     })
     .catch(e => {
@@ -60,7 +71,21 @@ function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (req.body) {
+    if (req.body.name) {
+      const minLength = 2
+      if (req.body.name.length >= minLength) {
+        next();
+      } else {
+        res.status(400).json({ message: `username must contain at least ${minLength} characters` })
+      }
+    } else {
+      res.status(400).json({message: "bad request: missing user name"})
+    }
+  }
+  else {
+    res.status(400).json({message: "bad reqeust: missing user data"})
+  }
 }
 
 function validatePost(req, res, next) {
