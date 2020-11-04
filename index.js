a19c81a75c15a61ba94825c673544d01f659d677
Kevin St.Onge <kevinstonge@gmail.com>
2020-11-04 08:54:39 -0500

    logger done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // code away!
+const server = require("./server.js");
+const PORT = 4000;
+server.listen(PORT, ()=>{console.log(`listening on port: ${PORT}`)})
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+function logger(req, res, next) {
+  console.log(`req.method: ${req.method}, req.url: ${req.url}, time: ${new Date()}`);
+  next();
+}
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
 
-function logger(req, res, next) {}
+
 
 module.exports = server;
