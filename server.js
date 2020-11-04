f92aa60ff704704a84f3aa838493db4cc7de9059
Kevin St.Onge <kevinstonge@gmail.com>
2020-11-04 09:25:23 -0500

    validateUserId working

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
 const server = express();
 server.use(express.json());
+const cors = require('cors');
+server.use(cors());
 function logger(req, res, next) {
   console.log(`req.method: ${req.method}, req.url: ${req.url}, time: ${new Date()}`);
   next();
 }
 server.use(logger);
+
+const userRouter = require("./users/userRouter.js");
+server.use("/api/users", userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,38 +2,61 @@ const express = require('express');
 
 const router = express.Router();
 
+const user = require('./userDb.js');
+
 router.post('/', (req, res) => {
-  // do your magic!
+  console.log('post -> new user')
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, (req, res) => {
+  console.log('post to user by id');
+  res.status(200).json({message:"hello"})
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  console.log('get users');
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  console.log('get user by id');
+  res.status(200).json({ message: "get user by id" });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  console.log('get posts by user id');
+  res.status(200).json({
+    message: "get posts by user id",
+    user: req.user
+  });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  console.log('delete user by id')
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  console.log('update user by id')
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  console.log('validateUserId');
+  user.getById(req.params.id)
+    .then(r => {
+      if (r && r.name && r.id) {
+        req.user = r;
+        next();
+      }
+      else {
+        res.status(400).json({message: "invalid user id"})
+      }
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(500).json({ message: `error retrieving the user's information` });
+    });
+
 }
 
 function validateUser(req, res, next) {
