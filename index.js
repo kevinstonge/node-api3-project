a99f2c227c1ee39ece381042ce9d31f009adb3bf
Kevin St.Onge <kevinstonge@gmail.com>
2020-11-05 21:39:34 -0500

    use .env for port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 require('dotenv').config();
 const server = require("./server.js");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const MOTD = process.env.MOTD || "default motd";
 server.listen(PORT, () => { console.log(`listening on port: ${PORT}`); console.log(MOTD)})
